9d6282d56b7175a94e3ba725429622238b90e233
shehanreshin <asp.shehanreshin@gmail.com>
2024-02-07 17:47:56 +0530

    Add items dynamically to table in the place order form

--- src/main/java/controller/PlaceOrderController.java
+++ src/main/java/controller/PlaceOrderController.java
@@ -1,14 +1,20 @@
 package controller;
 
+import db.CurrentOrder;
 import dto.ItemDto;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class PlaceOrderController implements Initializable {
@@ -64,6 +70,20 @@ public class PlaceOrderController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<ItemDto> currentOrder = CurrentOrder.getInstance().getCurrentOrder();
+        int total = 0;
+        for (ItemDto itemDto : currentOrder) {
+            total += itemDto.getStartingPrice();
+        }
+        lblTotal.setText(""+total);
 
+        colItem.setCellValueFactory(new PropertyValueFactory<>("name"));
+        colBasicCost.setCellValueFactory(new PropertyValueFactory<>("startingPrice"));
+        loadItemTable(currentOrder);
+    }
+
+    private void loadItemTable(List<ItemDto> currentOrder) {
+        ObservableList<ItemDto> observableList = FXCollections.observableArrayList(currentOrder);
+        tblItems.setItems(observableList);
     }
 }
