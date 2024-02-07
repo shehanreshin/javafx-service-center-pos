243cc7e84b3499db6ece86dec0cebb855fc7a7be
shehanreshin <asp.shehanreshin@gmail.com>
2024-02-07 16:49:32 +0530

    Add window icon

--- src/main/java/AppInitializer.java
+++ src/main/java/AppInitializer.java
@@ -1,6 +1,7 @@
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -20,6 +21,7 @@ public class AppInitializer extends Application {
         }
         primaryStage.setTitle("   Electroshack"); //spaces for margin
         primaryStage.setResizable(false);
+        primaryStage.getIcons().add(new Image(("./img/logo.png")));
         primaryStage.show();
     }
 }
--- src/main/java/controller/PlaceOrderController.java
+++ src/main/java/controller/PlaceOrderController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+
 public class PlaceOrderController {
+    public void closeButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void placeOrderButtonOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addCustomerButtonOnAction(ActionEvent actionEvent) {
+    }
 }
